cf24ff92cdba475b3945a495860e2fbe13b08f27
chaitnya96 <chaitnyaparikh@gmail.com>
2018-01-28 19:55:12 +0530

    second commit

--- sheduler/admin.py
+++ sheduler/admin.py
@@ -2,10 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Faculty,FType
+from .models import Faculty,Faculty_Type,Semester,Subject,Lecture,Timetable
 
 # Register your models here.
 admin.site.register(Faculty)
-admin.site.register(FType)
+admin.site.register(Faculty_Type)
+admin.site.register(Semester)
+admin.site.register(Subject)
+admin.site.register(Lecture)
+admin.site.register(Timetable)
 
 
--- sheduler/models.py
+++ sheduler/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 
-class FType(models.Model):
+class Faculty_Type(models.Model):
     faculty_type = models.CharField(max_length=250)
 
     def __str__(self):
@@ -13,7 +13,65 @@ class FType(models.Model):
 class Faculty(models.Model):
     Name = models.CharField(max_length=200)
     Qualification = models.CharField(max_length=300)
-    faculty_type = models.ForeignKey(FType)
+    faculty_type = models.ForeignKey(Faculty_Type)
 
     def __str__(self):
         return self.Name
+
+
+class Semester(models.Model):
+    sem_no = models.IntegerField()
+    division = models.CharField(max_length=1)
+    class_location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'Semester-'+str(self.sem_no) + ', ' + 'Division-' + str(self.division) + ', ' + str(self.class_location)
+
+
+class Subject(models.Model):
+    subject_code = models.CharField(max_length=50)
+    subject_name = models.CharField(max_length=250)
+    semester = models.ForeignKey(Semester)
+    faculty = models.ForeignKey(Faculty)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return self.subject_code + ' - ' + self.subject_name
+
+
+class Lecture(models.Model):
+    lecture_no = models.IntegerField()
+    from_time = models.TimeField()
+    to_time = models.TimeField()
+
+    def __str__(self):
+        return str(self.from_time) + '-  to  -' + str(self.to_time)
+
+
+class Timetable(models.Model):
+    Monday = 'Monday'
+    Tuesday = 'Tuesday'
+    Wednesday = 'Wednesday'
+    Thursday = 'Thursday'
+    Friday = 'Friday'
+    Saturday = 'Saturday'
+    Sunday = 'Sunday'
+
+    day_choice = (
+        (Monday, 'Monday'),
+        (Tuesday, 'Tuesday'),
+        (Wednesday, 'Wednesday'),
+        (Thursday, 'Thursday'),
+        (Friday, 'Friday'),
+        (Saturday, 'Saturday'),
+    )
+    day = models.CharField(
+        choices=day_choice,
+        max_length=10,
+    )
+    lecture = models.ForeignKey(Lecture)
+    semester = models.ForeignKey(Semester)
+    subject = models.ForeignKey(Subject)
+
+    def __str__(self):
+        return str(self.semester)
