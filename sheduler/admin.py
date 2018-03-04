2f0febadf78fb8517e379c769e745f6d2217c179
Pallav Parikh <Pallav@melvault.com>
2018-03-04 21:13:42 +0530

    changed custom user model, added is_staff

--- sheduler/admin.py
+++ sheduler/admin.py
@@ -2,14 +2,42 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Faculty, Semester, Subject, Lecture, Timetable, FacultyType
+from django.contrib.auth.admin import UserAdmin
+
+from models import *
+
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference the removed 'username' field
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Faculty)
 admin.site.register(FacultyType)
 admin.site.register(Semester)
 admin.site.register(Subject)
 admin.site.register(Lecture)
 admin.site.register(Timetable)
-
-
